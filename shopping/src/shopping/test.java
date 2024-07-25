56453e6be00cb931b69b9ff7241d67fdb17df332
tkvkdldj <dlgmldn00@gmail.com>
2024-07-25 15:27:10 +0900

    테스트

---
+++ shopping/src/shopping/test.java
@@ -0,0 +1,14 @@
+package shopping;
+
+public class test {
+
+	public static void main(String[] args) {
+		String mid = "홍길동";
+		System.out.println(mid);
+		int b = 10;
+		int c = 20;
+		System.out.println("코드 변경");
+		
+		String user = "hong";
+	}
+}
